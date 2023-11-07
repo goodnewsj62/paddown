450593b8b0f2c915083c6a20bec26e9f4a664c58
goodnews <goodnewsj62@gmail.com>
2023-11-07 04:35:55 +0100

    setup

---
+++ src/main/java/com/paddown/paddown/PaddownApplication.java
@@ -0,0 +1,13 @@
+package com.paddown.paddown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaddownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaddownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paddown/paddown/PaddownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paddown.paddown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaddownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
